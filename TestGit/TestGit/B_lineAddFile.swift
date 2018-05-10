0d85e9ce77b9c01c9290f87268c4e10fc6527d92
songkaiping <somnus.kp@qq.com>
2018-05-11 02:14:04 +0800

    B_line add first

---
+++ TestGit/TestGit/B_lineAddFile.swift
@@ -0,0 +1,29 @@
+//
+//  B_lineAddFile.swift
+//  TestGit
+//
+//  Created by superMac on 2018/5/11.
+//Copyright © 2018年 superMac. All rights reserved.
+//
+
+import UIKit
+
+class B_lineAddFile: NSObject {
+
+	//MARK: - *********** Liftcycle ***********
+
+    
+	//MARK: - *********** Variable ***********
+}
+
+//MARK: - Delegate
+extension B_lineAddFile{}
+
+//MARK: - Event Handle
+extension B_lineAddFile{}
+
+//MARK: - Private Method
+extension B_lineAddFile{}
+
+//MARK: - Public Method
+extension B_lineAddFile{}
