a2ef8bd7ff23b3b895b3fa0f9260cc507beb286a
songkaiping <somnus.kp@qq.com>
2018-05-11 02:16:35 +0800

    A_line first

---
+++ TestGit/TestGit/B_first_2.swift
@@ -0,0 +1,29 @@
+//
+//  B_first_2.swift
+//  TestGit
+//
+//  Created by superMac on 2018/5/11.
+//Copyright © 2018年 superMac. All rights reserved.
+//
+
+import UIKit
+
+class B_first_2: NSObject {
+
+	//MARK: - *********** Liftcycle ***********
+
+    
+	//MARK: - *********** Variable ***********
+}
+
+//MARK: - Delegate
+extension B_first_2{}
+
+//MARK: - Event Handle
+extension B_first_2{}
+
+//MARK: - Private Method
+extension B_first_2{}
+
+//MARK: - Public Method
+extension B_first_2{}
