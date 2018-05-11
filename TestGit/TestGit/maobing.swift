177778d45283fde7c5a2e6e7b7335a2ecaf38914
songkaiping <songkaiping@landsone.com>
2018-05-12 06:37:01 +0800

    maobing

---
+++ TestGit/TestGit/maobing.swift
@@ -0,0 +1,29 @@
+//
+//  maobing.swift
+//  TestGit
+//
+//  Created by superMac on 2018/5/12.
+//Copyright © 2018年 superMac. All rights reserved.
+//
+
+import UIKit
+
+class maobing: NSObject {
+
+	//MARK: - *********** Liftcycle ***********
+
+    
+	//MARK: - *********** Variable ***********
+}
+
+//MARK: - Delegate
+extension maobing{}
+
+//MARK: - Event Handle
+extension maobing{}
+
+//MARK: - Private Method
+extension maobing{}
+
+//MARK: - Public Method
+extension maobing{}
