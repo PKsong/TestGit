e2d23739ad6582a214b3c4db382f29f804e030ed
songkaiping <songkaiping@landsone.com>
2018-05-13 16:27:01 +0800

    icloud3

---
+++ TestGit/TestGit/TestICloud/TestICloud3.swift
@@ -0,0 +1,29 @@
+//
+//  TestICloud3.swift
+//  TestGit
+//
+//  Created by superMac on 2018/5/13.
+//Copyright © 2018年 superMac. All rights reserved.
+//
+
+import UIKit
+
+class TestICloud3: NSObject {
+
+	//MARK: - *********** Liftcycle ***********
+
+    
+	//MARK: - *********** Variable ***********
+}
+
+//MARK: - Delegate
+extension TestICloud3{}
+
+//MARK: - Event Handle
+extension TestICloud3{}
+
+//MARK: - Private Method
+extension TestICloud3{}
+
+//MARK: - Public Method
+extension TestICloud3{}
