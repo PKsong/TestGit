080ccb642630539152ff83eb37b252d2c6563dd4
songkaiping <songkaiping@landsone.com>
2018-05-13 16:22:25 +0800

    icloud 2

---
+++ TestGit/TestGit/TestICloud/TestICloud2.swift
@@ -0,0 +1,29 @@
+//
+//  TestICloud2.swift
+//  TestGit
+//
+//  Created by superMac on 2018/5/13.
+//Copyright © 2018年 superMac. All rights reserved.
+//
+
+import UIKit
+
+class TestICloud2: NSObject {
+
+	//MARK: - *********** Liftcycle ***********
+
+    
+	//MARK: - *********** Variable ***********
+}
+
+//MARK: - Delegate
+extension TestICloud2{}
+
+//MARK: - Event Handle
+extension TestICloud2{}
+
+//MARK: - Private Method
+extension TestICloud2{}
+
+//MARK: - Public Method
+extension TestICloud2{}
